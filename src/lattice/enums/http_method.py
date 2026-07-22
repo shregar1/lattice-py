fb25b9fa5045391fca7e5e10906e49a16b9a62f7
Dev <dev@example.com>
2026-07-22 23:15:44 +0530

    feat: add http_status and http_method constants and enums

---
+++ src/lattice/enums/http_method.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class HttpMethod(str, Enum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
